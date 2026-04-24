1e1f5186553dd59e59e7322083669ebe93ceb979
victorcbjr100-oss <victorcbjr100@gmail.com>
2026-04-24 18:06:14 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-client'
+import { createClient } from '@supabase/supabase-js'
 
-// Substitua pelos valores reais que você copiou do painel do Supabase
-const supabaseUrl = 'https://lpbeuehxuadmgsbsfmcu.supabase.co/rest/v1/' 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwYmV1ZWh4dWFkbWdzYnNmbWN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NzQ5MzcsImV4cCI6MjA5MjU1MDkzN30.Pi0p6Fwt4j6BxmLTlDWWKwafOv3cbW3byMP-XbeEHxM' 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
